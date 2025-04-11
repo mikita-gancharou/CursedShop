90a127065a9d3b399ab6c44e4401dffd23bb16cb
mikita-gancharou <mikita.gancharou@gmail.com>
2025-04-11 17:25:10 -0300

    flying eye added

---
+++ Mobs/FlyingEye/flying_eye.gd
@@ -0,0 +1,43 @@
+# Flying.gd
+class_name Flying
+extends CharacterBody2D
+
+@export var speed: float = 100.0
+@export var acceleration: float = 0.25
+@export var gravity: float = 500.0
+@export var damage: int = 20
+
+var health: int = 100
+
+@onready var animplayer: AnimationPlayer = $AnimationPlayer
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var attack_area: Area2D = $AttackDirection/AttackArea
+
+var last_player_position: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	$AttackDirection/HitBox/CollisionShape2D.disabled = true
+
+func _process(_delta: float) -> void:
+	pass
+
+func get_input_vector() -> Vector2:
+	var input_vector = Vector2.ZERO
+	return input_vector
+
+func apply_gravity(delta: float) -> void:
+	velocity.y += gravity * delta
+
+func apply_movement(input_vector: Vector2, _delta: float) -> void:
+	velocity.x = lerp(velocity.x, input_vector.x * speed, acceleration)
+
+func apply_velocity(_delta: float) -> void:
+	move_and_slide()
+
+func change_direction(direction) -> void:
+	if sign(direction) == -1:
+		sprite.flip_h = true
+		$AttackDirection.rotation_degrees = 180
+	elif sign(direction) == 1:
+		sprite.flip_h = false
+		$AttackDirection.rotation_degrees = 0
